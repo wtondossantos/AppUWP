af3b66230be9622bbb1b6673881ce893c05c5e96
Wellington <wellington@wton.com.br>
2018-01-13 17:07:04 -0200

    class banco

---
+++ AppUWP/Data/Banco.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppUWP.Data
+{
+    public class Banco
+    {
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        //[Key]
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
